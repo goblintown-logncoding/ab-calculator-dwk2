6dfc57766206e864c898c1a7076b4bcfd41f4e05
Daewoong <kodw.kor@gmail.com>
2024-08-01 21:15:38 +0900

    feat: add Header, Content, Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import './App.css';
+import Header from './Header';
+import Footer from "./Footer";
+import Content from "./Content";
 
-function App() {
-
+const App = () => {
   return (
     <>
+      <Header />
+      <Content />
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
